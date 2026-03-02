2b1587441720f328e78825a7a5f71fc076ee89e6
Saavedra Ramon <102835876+Papyrus-Saa@users.noreply.github.com>
2026-03-02 17:11:05 +0100

    Add FeaturedProperty and TripleSliderProps interfaces for triple slider component

---
+++ src/components/features/triple-slider/TripleSlider.types.ts
@@ -0,0 +1,18 @@
+export interface FeaturedProperty {
+  id: string;
+  title: string;
+  type: string;
+  price: string;
+  location: string;
+  imageUrl: string;
+  slug: string;
+  area?: string;
+  rooms?: number;
+}
+
+export interface TripleSliderProps {
+  properties: FeaturedProperty[];
+  speed?: number;
+  autoplay?: boolean;
+  autoplayDelay?: number;
+}
