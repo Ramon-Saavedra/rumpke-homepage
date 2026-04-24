7cc9da0890ef63a08932a3dd7944beb6bc261afe
Saavedra Ramon <102835876+Papyrus-Saa@users.noreply.github.com>
2026-04-24 08:57:03 +0200

    chore: update React and ReactDOM to version 19.2.5, add Jest config for Next.js, and declare Swiper CSS module types

---
+++ jest.config.mjs
@@ -0,0 +1,16 @@
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+};
+
+export default createJestConfig(customJestConfig);
